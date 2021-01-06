a664543b2417440a2d8cdab7ff6041221bed2bf6
serii <seriiburduja@gmail.com>
2021-01-06 10:17:15 +0200

    form login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,15 @@
 <template>
-<h2>Login</h2>
+<div class="form">
+  <h2 class="form__title">Login</h2>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+  </div>
+</div>
 </template>
 
 <script>
