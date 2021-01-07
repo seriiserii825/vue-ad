88e8eedf2de1758901ea043f603dcc01d3092e37
serii <seriiburduja@gmail.com>
2021-01-07 10:54:49 +0200

    validate

--- src/components/form/FormGroup.vue
+++ src/components/form/FormGroup.vue
@@ -2,22 +2,40 @@
   <div class="form__group" :class="{'active': inputFocus}">
     <label :for="type">{{ label }}</label>
     <font-awesome-icon :icon="['fas',icon]"/>
-    <input @focusin="inputFocus = true" @input="checkInput($event)" @focusout="inputFocus = false" :type="type" :name="type" :id="type">
+    <input @focusin="inputFocus = true" @input="checkInput($event)" @focusout="checkFocus($event)" :type="type" :name="type" :id="type">
+    <p class="field-error" v-if="errorMessage.required">{{ validateInput }}</p>
   </div>
 </template>
 
 <script>
 
 export default {
-  props: ['type', 'label', 'icon', 'value'],
+  props: ['type', 'label', 'icon', 'errorMessage'],
   data () {
     return {
-      inputFocus: false
+      inputFocus: false,
+      inputEmpty: true
     }
   },
   methods: {
-    checkInput (value) {
-      this.$emit('input', value)
+    checkInput (e) {
+      this.$emit('input', e.target.value)
+    },
+    checkFocus (e) {
+      if (e.target.value === '') {
+        this.inputFocus = false
+      }
+    }
+  },
+  computed: {
+    validateInput () {
+      if (this.errorMessage.required) {
+        return this.errorMessage.required
+      } else if (this.errorMessage.email) {
+        return this.errorMessage.email
+      } else {
+        return ''
+      }
     }
   }
 }
@@ -26,5 +44,11 @@ export default {
 
 <style lang="scss">
 @import "src/assets/scss/variables";
-
+.field-error {
+  position: absolute;
+  bottom: -2rem;
+  left: 4.5rem;
+  font-size: 1.2rem;
+  color: red;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,9 +3,9 @@
     <div class="login__form">
       <Form>
         <h2 class="form__title" slot="title">Form login</h2>
-        <FormGroup @input="validateInput" v-model="email" slot="form-group-1" type="email" label="Email" icon="user"></FormGroup>
-        <FormGroup v-model="password" slot="form-group-2" type="password" label="Password" icon="lock"></FormGroup>
-        <Button type="submit" label="Login" slot="submit"></Button>
+        <FormGroup :errorMessage="{email: errors.email.email, required: errors.email.email}" @input="checkInput" v-model="email" slot="form-group-1" type="email" label="Email" icon="user"></FormGroup>
+        <FormGroup :errorMessage="{required: errors.password.required}" v-model="password" slot="form-group-2" type="password" label="Password" icon="lock"></FormGroup>
+        <Button @click.native="checkForm" type="submit" label="Login" slot="submit"></Button>
       </Form>
     </div>
   </div>
@@ -15,13 +15,21 @@
 import Form from '@/components/form/Form'
 import FormGroup from '@/components/form/FormGroup'
 import Button from '@/components/ui/Button'
-import { validationMixin } from 'vuelidate'
 
 export default {
   data () {
     return {
-      email: '',
-      password: ''
+      errors: {
+        email: {
+          required: '',
+          email: ''
+        },
+        password: {
+          required: ''
+        }
+      },
+      email: null,
+      password: null
     }
   },
   components: {
@@ -30,11 +38,28 @@ export default {
     Button
   },
   methods: {
-    validateInput () {
-      console.log(this.email)
+    checkInput: function () {
+      if (!this.validEmail(this.email)) {
+        this.errors.email.email = 'Email address is incorrect'
+      }
+    },
+    validEmail: function (email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    checkForm () {
+      if (!this.email) {
+        this.errors.email.required = 'This field is required'
+      } else {
+        this.errors.email.required = ''
+      }
+      if (!this.password) {
+        this.errors.password.required = 'This field is required'
+      } else {
+        this.errors.password.required = ''
+      }
     }
-  },
-  mixins: [validationMixin]
+  }
 }
 </script>
 
