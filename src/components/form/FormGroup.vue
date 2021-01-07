4fbb3fcf87b572f88e85854b3072cfd9e43dfab6
serii <seriiburduja@gmail.com>
2021-01-07 09:15:51 +0200

    email

--- src/components/form/FormGroup.vue
+++ src/components/form/FormGroup.vue
@@ -2,20 +2,26 @@
   <div class="form__group" :class="{'active': inputFocus}">
     <label :for="type">{{ label }}</label>
     <font-awesome-icon :icon="['fas',icon]"/>
-    <input @focusin="inputFocus = true" @focusout="inputFocus = false" :type="type" :name="type" :id="type">
+    <input @focusin="inputFocus = true" @input="checkInput($event)" @focusout="inputFocus = false" :type="type" :name="type" :id="type">
   </div>
 </template>
 
 <script>
 
 export default {
-  props: ['type', 'label', 'icon'],
+  props: ['type', 'label', 'icon', 'value'],
   data () {
     return {
       inputFocus: false
     }
+  },
+  methods: {
+    checkInput (value) {
+      this.$emit('input', value)
+    }
   }
 }
+
 </script>
 
 <style lang="scss">
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,8 @@
     <div class="login__form">
       <Form>
         <h2 class="form__title" slot="title">Form login</h2>
-        <FormGroup slot="form-group-1" type="email" label="Email" icon="user"></FormGroup>
-        <FormGroup slot="form-group-2" type="password" label="Password" icon="lock"></FormGroup>
+        <FormGroup @input="validateInput" v-model="email" slot="form-group-1" type="email" label="Email" icon="user"></FormGroup>
+        <FormGroup v-model="password" slot="form-group-2" type="password" label="Password" icon="lock"></FormGroup>
         <Button type="submit" label="Login" slot="submit"></Button>
       </Form>
     </div>
@@ -15,13 +15,26 @@
 import Form from '@/components/form/Form'
 import FormGroup from '@/components/form/FormGroup'
 import Button from '@/components/ui/Button'
+import { validationMixin } from 'vuelidate'
 
 export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   components: {
     Form,
     FormGroup,
     Button
-  }
+  },
+  methods: {
+    validateInput () {
+      console.log(this.email)
+    }
+  },
+  mixins: [validationMixin]
 }
 </script>
 
