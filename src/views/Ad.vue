6c5e53f8c934cb0513e03f8e749e70054842af1a
serii <seriiburduja@gmail.com>
2021-01-10 16:13:55 +0200

    single ad

--- src/views/Ad.vue
+++ src/views/Ad.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    <h2>Ad</h2>
+  <div class="ad">
+    <ImageAbsolute v-image-height="'60rem'" :img-src="require(`@/assets/images/products/1.jpg`)"></ImageAbsolute>
+    <div class="cards-h__body">
+      <div class="cards-h__content">
+        <h2 class="cards-h__title">Title</h2>
+        <p class="cards-h__text">Text</p>
+      </div>
+    </div>
+    <div class="cards-h__footer">
+      <Button label="Edit" class="btn btn--pink btn--hover"></Button>
+      <Button label="Buy" class="btn btn--blue btn--hover"></Button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import ImageAbsolute from '@/components/ui/ImageAbsolute'
+import Button from '@/components/ui/Button'
+
+export default {
+  components: {
+    ImageAbsolute,
+    Button
+  }
+}
 </script>
 
 <style lang="scss">
 @import "src/assets/scss/variables";
-
+.cards-h__footer {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
