70621d5a88f338416a5f2039b4497401edf25afa
serii <seriiburduja@gmail.com>
2021-01-11 10:52:36 +0200

    problem with indentation

--- src/main.js
+++ src/main.js
@@ -3,28 +3,27 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuelidate from 'vuelidate'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faUser, faLock } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faUserSecret, faUser, faLock} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import VueSplide from '@splidejs/vue-splide'
 import '@splidejs/splide/dist/css/themes/splide-default.min.css'
 import GridDirective from '@/directives/grid'
 import ImageHeightDirective from '@/directives/image-height'
+import ImageMediaHeightDirective from '@/directives/image-media-query-height'
 
 library.add(faUserSecret)
 library.add(faUser)
 library.add(faLock)
-
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(Vuelidate)
 Vue.use(VueSplide)
 Vue.directive('grid-columns', GridDirective)
 Vue.directive('image-height', ImageHeightDirective)
-
+Vue.directive('image-media-height', ImageMediaHeightDirective)
 Vue.config.productionTip = false
-
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
